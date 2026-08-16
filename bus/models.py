c21acb81eb00cd767d25b87cd0d5f5311ea2334d
nandha gopal <nandagopaldd143@gmail.com>
2026-08-17 00:46:35 +0530

    Add GPS active status and location timestamp

--- bus/models.py
+++ bus/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
 class Bus(models.Model):
+
     bus_number = models.CharField(max_length=20)
+
     time = models.TimeField()
+
     destination = models.CharField(max_length=200)
+
     latitude = models.FloatField(default=0)
+
     longitude = models.FloatField(default=0)
+
+    # GPS last updated time
+    location_updated_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    # GPS currently running or not
+    gps_active = models.BooleanField(
+        default=False
+    )
 
     def __str__(self):
         return self.bus_number
