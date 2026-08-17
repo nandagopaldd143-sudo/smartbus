fb76437b99d2329d3d770b8711747fbd898831b5
nandha gopal <nandagopaldd143@gmail.com>
2026-08-17 19:49:53 +0530

    Fix driver dashboard redirect

--- bus/views.py
+++ bus/views.py
@@ -28,7 +28,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("home")
+            return redirect("driver_dashboard")
 
         return render(
             request,
